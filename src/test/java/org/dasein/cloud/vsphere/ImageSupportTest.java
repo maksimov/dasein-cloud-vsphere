0897f03b475372403d3798a82f38b28ef70e6cb4
Roger Unwin <roger.unwin@enstratius.com>
2015-09-15 14:56:07 -0700

    conversion to jmockit

--- src/test/java/org/dasein/cloud/vsphere/ImageSupportTest.java
+++ src/test/java/org/dasein/cloud/vsphere/ImageSupportTest.java
@@ -1,14 +1,15 @@
 package org.dasein.cloud.vsphere;
 
-
+/*
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyList;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
+*/
+
+import static org.junit.Assert.*;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.dasein.cloud.CloudException;
@@ -16,21 +17,25 @@ import org.dasein.cloud.InternalException;
 import org.dasein.cloud.compute.ImageFilterOptions;
 import org.dasein.cloud.compute.MachineImage;
 import org.dasein.cloud.vsphere.compute.server.ImageSupport;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vmware.vim25.InvalidPropertyFaultMsg;
 import com.vmware.vim25.ManagedObjectReference;
 import com.vmware.vim25.PropertyFilterSpec;
 import com.vmware.vim25.RetrieveOptions;
 import com.vmware.vim25.RetrieveResult;
 import com.vmware.vim25.RuntimeFaultFaultMsg;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.codehaus.jackson.annotate.JsonAutoDetect.Visibility;
+import org.codehaus.jackson.annotate.JsonMethod;
+import org.codehaus.jackson.map.DeserializationConfig.Feature;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectMapper.DefaultTyping;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import mockit.*;
 
 /**
  * User: rogerunwin
@@ -39,90 +44,77 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RunWith(JUnit4.class)
 public class ImageSupportTest {
 
-    private ManagedObjectReference getMockViewManager() {
-        ObjectMapper mapper = new ObjectMapper();
-        ManagedObjectReference mor = null;
-        try {
-            mor = mapper.readValue(new File("src/test/resources/ImageSupport/viewManager.json"), ManagedObjectReference.class);
-        } catch ( Exception e ) { }
-        return mor;
-    }
-
-    private ManagedObjectReference getMockRootFolder() {
-        ObjectMapper mapper = new ObjectMapper();
-        ManagedObjectReference rf = null;
-        try {
-            rf = mapper.readValue(new File("src/test/resources/ImageSupport/rootFolder.json"), ManagedObjectReference.class);
-        } catch ( Exception e ) { }
-        return rf;
-    }
-
-    private ManagedObjectReference createMockContainerView() {
-        ObjectMapper mapper = new ObjectMapper();
-        ManagedObjectReference mor = null;
-        try {
-            mor = mapper.readValue(new File("src/test/resources/ImageSupport/containerView.json"), ManagedObjectReference.class);
-        } catch ( Exception e ) { }
-        return mor;
-    }
-
-    private ManagedObjectReference getMockPropertyCollector() {
-        ObjectMapper mapper = new ObjectMapper();
-        ManagedObjectReference mor = null;
-        try {
-            mor = mapper.readValue(new File("src/test/resources/ImageSupport/propertyCollector.json"), ManagedObjectReference.class);
-        } catch ( Exception e ) { }
-        return mor;
-    }
-
-    private RetrieveResult getMockRetrievePropertiesEx() {
-        ObjectMapper mapper = new ObjectMapper();
-        RetrieveResult rr = null;
-        try {
-            rr = mapper.readValue(new File("src/test/resources/ImageSupport/propertiesEx.json"), RetrieveResult.class);
-        } catch ( Exception e ) { }
-        return rr;
-    }
-
     @Test
-    public void listImagesTest() {
-        ImageSupport imageSupport = mock(ImageSupport.class, CALLS_REAL_METHODS);
+    public void testBankProcessAccount() {
         ImageFilterOptions options = ImageFilterOptions.getInstance();
-        doReturn(getMockViewManager()).when(imageSupport).getViewManager();
-        doReturn(getMockRootFolder()).when(imageSupport).getRootFolder();
-        
-        try {
-            doReturn(createMockContainerView()).when(imageSupport).createContainerView(any(ManagedObjectReference.class), any(ManagedObjectReference.class), anyList(), eq(true));
-        } catch ( RuntimeFaultFaultMsg e ) { 
-            System.out.println(e);
-        }
+        ImageSupport imageSupport = new ImageSupport();
 
-        doReturn(getMockPropertyCollector()).when(imageSupport).getPropertyCollector();
+        new MockUp<ImageSupport>() {
 
-        try {
-            doReturn(getMockRetrievePropertiesEx()).when(imageSupport).retrievePropertiesEx(any(ManagedObjectReference.class), anyListOf(PropertyFilterSpec.class), any(RetrieveOptions.class));
-        } catch ( RuntimeFaultFaultMsg e ) {
-            e.printStackTrace();
-        } catch ( InvalidPropertyFaultMsg e ) { 
-            e.printStackTrace();
-        }
+            @Mock
+            public ManagedObjectReference getViewManager(){
+                ObjectMapper mapper = new ObjectMapper();
+                ManagedObjectReference mor = null;
+                try {
+                    mor = mapper.readValue(new File("src/test/resources/ImageSupport/viewManager.json"), ManagedObjectReference.class);
+                } catch ( Exception e ) { }
+                return mor;
+            }
+
+            @Mock
+            private ManagedObjectReference getRootFolder() {
+                ObjectMapper mapper = new ObjectMapper();
+                ManagedObjectReference rf = null;
+                try {
+                    rf = mapper.readValue(new File("src/test/resources/ImageSupport/rootFolder.json"), ManagedObjectReference.class);
+                } catch ( Exception e ) { }
+                return rf;
+            }
+
+            @Mock
+            private ManagedObjectReference createContainerView(ManagedObjectReference viewManager, ManagedObjectReference rootFolder, List<String> vmList, boolean b) {
+                ObjectMapper mapper = new ObjectMapper();
+                ManagedObjectReference mor = null;
+                try {
+                    mor = mapper.readValue(new File("src/test/resources/ImageSupport/containerView.json"), ManagedObjectReference.class);
+                } catch ( Exception e ) { }
+                return mor;
+            }
+
+            @Mock
+            private ManagedObjectReference getPropertyCollector() {
+                ObjectMapper mapper = new ObjectMapper();
+                ManagedObjectReference mor = null;
+                try {
+                    mor = mapper.readValue(new File("src/test/resources/ImageSupport/propertyCollector.json"), ManagedObjectReference.class);
+                } catch ( Exception e ) { }
+                return mor;
+            }
+
+            @Mock
+            private RetrieveResult retrievePropertiesEx(ManagedObjectReference propColl, List<PropertyFilterSpec> fSpecList, RetrieveOptions ro) {
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.configure(Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                mapper.enableDefaultTypingAsProperty(DefaultTyping.OBJECT_AND_NON_CONCRETE, "type");
+                RetrieveResult rr = null;
+                try {
+                    rr = mapper.readValue(new File("src/test/resources/ImageSupport/propertiesEx.json"), RetrieveResult.class);
+                } catch ( Exception e ) { }
+                return rr;
+            }
+        };
 
         Iterable<MachineImage> result = null;
         try {
-            //when(imageSupport.listImages(any(ImageFilterOptions.class))).thenCallRealMethod();
-
             result = imageSupport.listImages(options);
         } catch ( CloudException e ) {
             e.printStackTrace();
         } catch ( InternalException e ) {
             e.printStackTrace();
         }
+
+        assertNotNull("return should not be null", result);
     }
-
-
-
-
-
 
 
 }
