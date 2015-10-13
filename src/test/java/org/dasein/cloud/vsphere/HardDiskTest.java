32feff708f7bc2e4df7e66b171f1f3d759926883
Danielle Mayne <danielle.mayne@enstratus.com>
2015-10-13 10:49:02 +0100

    volume tests passing

--- src/test/java/org/dasein/cloud/vsphere/HardDiskTest.java
+++ src/test/java/org/dasein/cloud/vsphere/HardDiskTest.java
@@ -5,6 +5,9 @@ import com.vmware.vim25.PropertyChange;
 import com.vmware.vim25.PropertySpec;
 import com.vmware.vim25.RetrieveResult;
 import mockit.Expectations;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
 import org.dasein.cloud.CloudException;
 import org.dasein.cloud.InternalException;
 import org.dasein.cloud.compute.Volume;
@@ -26,7 +29,7 @@ import java.util.List;
  */
 public class HardDiskTest extends VsphereTestBase {
     private ObjectManagement om = new ObjectManagement();
-    private final RetrieveResult hardDisks = om.readJsonFile("src/test/resources/HardDisk/hardDisks.json", RetrieveResult.class);
+    private RetrieveResult hardDisks = om.readJsonFile("src/test/resources/HardDisk/hardDisks.json", RetrieveResult.class);
     private final ResourcePool[] resourcePools = om.readJsonFile("src/test/resources/HardDisk/resourcePools.json", ResourcePool[].class);
     private final RetrieveResult datastores = om.readJsonFile("src/test/resources/HardDisk/datastores.json", RetrieveResult.class);
     private final StoragePool[] storagePools = om.readJsonFile("src/test/resources/HardDisk/storagePools.json", StoragePool[].class);
@@ -45,6 +48,10 @@ public class HardDiskTest extends VsphereTestBase {
         method = new VsphereMethod(vsphereMock);
         hardDiskPSpec = hd.getHardDiskPSpec();
         datastorePSpec = hd.getDatastorePropertySpec();
+        ObjectManagement om = new ObjectManagement();
+        om.mapper.enableDefaultTypingAsProperty(ObjectMapper.DefaultTyping.NON_FINAL, "type");
+        om.mapper.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS, false);
+        hardDisks = om.readJsonFile("src/test/resources/HardDisk/hardDisks.json", RetrieveResult.class);
     }
 
     @Test
@@ -83,13 +90,17 @@ public class HardDiskTest extends VsphereTestBase {
         assertTrue("Empty volume list returned", volumes.iterator().hasNext());
         Volume volume = volumes.iterator().next();
         assertNotNull("Null volume found", volume);
-        assertEquals("jonshaun.vmdk", volume.getProviderVolumeId());
-        assertEquals("jonshaun.vmdk", volume.getName());
+        assertEquals("dmTesting7Oct_2.vmdk", volume.getProviderVolumeId());
+        assertEquals("Hard disk 1", volume.getName());
         assertEquals("datacenter-21", volume.getProviderRegionId());
-        assertEquals("domain-c26", volume.getProviderDataCenterId());
-        assertEquals("jonshaun.vmdk", volume.getDescription());
-        assertNull("Standalone vmdk file should not have a vm id", volume.getProviderVirtualMachineId());
-        assertNull(volume.getSize());
+        assertEquals("52,428,800 KB", volume.getDescription());
+        assertEquals("vm-2318", volume.getProviderVirtualMachineId());
+        assertEquals("0", volume.getDeviceId());
+        assertEquals(52, volume.getSizeInGigabytes());
+        assertTrue(volume.isRootVolume());
+        assertNotNull(volume.getTags());
+        String filePath = volume.getTag("filePath");
+        assertEquals("[shared-datastore-1] dmTesting7Oct/dmTesting7Oct_2.vmdk", filePath);
     }
 
     @Test
@@ -123,17 +134,19 @@ public class HardDiskTest extends VsphereTestBase {
             }
         };
 
-        Volume volume = hd.getVolume("dmTesting7Oct_2.vmdk");
+        Volume volume = hd.getVolume("dmTesting7Oct_1.vmdk");
         assertNotNull("Valid volume object should not be null", volume);
-        assertEquals("dmTesting7Oct_2.vmdk", volume.getProviderVolumeId());
-        assertEquals("Hard disk 1", volume.getName());
+        assertEquals("dmTesting7Oct_1.vmdk", volume.getProviderVolumeId());
+        assertEquals("dmTesting7Oct_1.vmdk", volume.getName());
+        assertEquals("domain-c26", volume.getProviderDataCenterId());
         assertEquals("datacenter-21", volume.getProviderRegionId());
-        assertEquals("52,428,800 KB", volume.getDescription());
-        assertNull("vm-2318", volume.getProviderVirtualMachineId());
-        assertEquals("0", volume.getDeviceId());
-        assertEquals(52, volume.getSizeInGigabytes());
+        assertEquals("dmTesting7Oct_1.vmdk", volume.getDescription());
+        assertNull("Standalone file should not have vm id", volume.getProviderVirtualMachineId());
+        assertNull("Standalone file should not have device id", volume.getDeviceId());
+        assertEquals(0, volume.getSizeInGigabytes());
         assertNotNull(volume.getTags());
         String filePath = volume.getTag("filePath");
-        assertEquals("[shared-datastore-1] dmTesting7Oct/dmTesting7Oct_2.vmdk", filePath);
+        assertEquals("[shared-datastore-1] dmTesting7Oct/dmTesting7Oct_1.vmdk", filePath);
+        assertFalse(volume.isRootVolume());
     }
 }
